a8860ce8e9372561e25759bff8c872c104ed8f90
Wes Higbee <wes.mcclure@gmail.com>
2026-03-04 20:28:53 -0600

    release reminder abbrs

---
+++ .config.fish
@@ -0,0 +1,6 @@
+abbr build "uv build"
+abbr bump_patch "uvx bump-my-version bump patch; git push && git push --tags"
+function get_pyproject_current_version
+    cat pyproject.toml | grep current_version | string match --regex "\d+\.\d+\.\d+"
+end
+abbr create_release "gh release create v$(get_pyproject_current_version) --title 'v$(get_pyproject_current_version)'"
